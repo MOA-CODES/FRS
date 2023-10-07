33153245c493ee8ec04b90cf8477915e3318ed38
MOA -CODES <moyo.ajla@gmail.com>
2023-10-07 18:07:22 +0100

    middleware

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 require('dotenv').config()
+require('express-async-errors')
+
+const errorHandler = require('./middleware/error-handler')
+const notFound = require('./middleware/not-found')
+const auth = require('./middleware/auth')
 
 const conn = require('./db/conn')
 const express = require('express');
@@ -9,6 +14,9 @@ const port = process.env.PORT || 3031
 app.get('/', (req, res) => {
     res.send('Friend Request System App')
 })
+
+app.use(notFound)
+app.use(errorHandler)
 
 conn()
 
